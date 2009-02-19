f9fa70958f7e845b9033a16ae5f65fcf90e6eb0d
bme@earthdev.burningman.com <bme@earthdev.burningman.com>
2009-02-19 01:19:04 +0000

    Fixed the profile photos thumbnails and added a sample_local_settings file

---
+++ sample_local_settings.py
@@ -0,0 +1,8 @@
+PINAX_ROOT='/home/user/bmedev/src/pinax'
+
+DATABASE_ENGINE = 'postgresql_psycopg2'  
+DATABASE_NAME = 'bme'
+DATABASE_USER = 'user'      
+DATABASE_PASSWORD = 'secret'    
+
+DEBUG=True
