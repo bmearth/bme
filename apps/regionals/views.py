7957bc1051ff198ba485f6651c3065b76098a82c
Jeffrey Johnson <ortelius@gmail.com>
2009-02-28 11:29:15 -0800

    Removed title and description

--- apps/regionals/views.py
+++ apps/regionals/views.py
@@ -10,14 +10,9 @@ from django.conf import settings
 from regionals.models import Regional
 
 def regionals(request, template_name="regionals/regionals.html", order=None):
-    regionals = Regional.objects.filter(deleted=False)
-    search_terms = request.GET.get('search', '')
-    if search_terms:
-        regionals = (regionals.filter(name__icontains=search_terms) |
-            regionals.filter(description__icontains=search_terms))
+    regionals = Regional.objects.all()
     context = {
         'regionals': regionals,
-        'search_terms': search_terms,
         'order': order,
     }
     return render_to_response(
