719fb146992ceb3fe9c8a6f807aea572a607a85f
Jeffrey Johnson <ortelius@gmail.com>
2009-02-28 11:37:23 -0800

    added null=True and blank=True to regionals.models

--- apps/regionals/models.py
+++ apps/regionals/models.py
@@ -5,11 +5,11 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 
 class Regional(Tribe):
-    contacts = models.ManyToManyField(User)
-    quote = models.TextField(null=True)
-    email = models.EmailField(null=True)
-    announcment_list = models.EmailField(null=True)
-    discussion_list = models.EmailField(null=True)
+    contacts = models.ManyToManyField(User, null=True, blank=True)
+    quote = models.TextField(null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    announcment_list = models.EmailField(null=True, blank=True)
+    discussion_list = models.EmailField(null=True, blank=True)
     # geo stuff
-    location = models.PointField(null=True)
-    location_text = models.CharField(max_length=80, unique=True, null=True)
+    location = models.PointField(null=True, blank=True)
+    location_text = models.CharField(max_length=80, unique=True, null=True, blank=True)
