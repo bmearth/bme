1f898e3a5453b0a64bc56216668c73645960bd33
ingenieroariel@crispeta.local <ingenieroariel@crispeta.local>
2009-05-24 20:21:35 -0500

    Added Anonymous access when there are no OAuth tokens

--- apps/api/handlers.py
+++ apps/api/handlers.py
@@ -1,4 +1,4 @@
-from piston.handler import BaseHandler
+from piston.handler import BaseHandler, AnonymousBaseHandler
 from piston.emitters import Emitter, JSONEmitter
 from brc.models import *
 from api.emitters import GeoJSONEmitter
@@ -6,7 +6,7 @@ from api.emitters import GeoJSONEmitter
 JSONEmitter.unregister('json')
 Emitter.register('json', GeoJSONEmitter, content_type='text/javascript; charset=utf-8')
 
-class ArtInstallationHandler(BaseHandler):
+class AnonymousArtInstallationHandler(BaseHandler):
 	allow_methods = ('GET',)
 	model = ArtInstallation 
 	fields = ('id', 'name', 'year', 'slug', 'artist', 'description', 'url', 'contact_email', 'location_point', 'location_poly', 'circular_street', 'time_address')
@@ -16,7 +16,18 @@ class ArtInstallationHandler(BaseHandler):
 		base = ArtInstallation.objects
 		return base.all()
 
-class PlayaEventHandler(BaseHandler):
+
+class ArtInstallationHandler(BaseHandler):
+	allow_methods = ('GET',)
+	model = ArtInstallation 
+	fields = ('id', 'name', 'year', 'slug', 'artist', 'description', 'url', 'contact_email', 'location_point', 'location_poly', 'circular_street', 'time_address')
+	anonymous = AnonymousArtInstallationHandler
+	@classmethod
+	def read(self, request):
+		base = ArtInstallation.objects
+		return base.all()
+
+class AnonymousPlayaEventHandler(AnonymousBaseHandler):
 	allow_methods = ('GET',)
 	model = PlayaEvent 
 	fields = ('id', 'name', 'year', 'description', 'url', 'contact_email', 'hometown', 'location_point', 'location_poly', 'circular_street', 'time_address', 'participants')
@@ -26,7 +37,19 @@ class PlayaEventHandler(BaseHandler):
 		base = PlayaEvent.objects
 		return base.all()
 
-class ThemeCampHandler(BaseHandler):
+
+class PlayaEventHandler(BaseHandler):
+	allow_methods = ('GET',)
+	model = PlayaEvent 
+	fields = ('id', 'name', 'year', 'description', 'url', 'contact_email', 'hometown', 'location_point', 'location_poly', 'circular_street', 'time_address', 'participants')
+	anonymous = AnonymousPlayaEventHandler
+	@classmethod
+	def read(self, request):
+		base = PlayaEvent.objects
+		return base.all()
+
+
+class AnonymousThemeCampHandler(AnonymousBaseHandler):
 	allow_methods = ('GET',)
 	model = ThemeCamp 
 	fields = ('id', 'year', 'name', 'description', 'type', 'start_date_time', 'end_date_time', 'duration', 'repeats', 'hosted_by_camp', 'located_at_art', 'location_point', 'location_poly', 'url', 'contact_email') 
@@ -36,7 +59,18 @@ class ThemeCampHandler(BaseHandler):
 		base = ThemeCamp.objects
 		return base.all()
 
-class CircularStreetHandler(BaseHandler):
+
+class ThemeCampHandler(BaseHandler):
+	allow_methods = ('GET',)
+	model = ThemeCamp 
+	fields = ('id', 'year', 'name', 'description', 'type', 'start_date_time', 'end_date_time', 'duration', 'repeats', 'hosted_by_camp', 'located_at_art', 'location_point', 'location_poly', 'url', 'contact_email') 
+	anonymous = AnonymousThemeCampHandler
+	@classmethod
+	def read(self, request):
+		base = ThemeCamp.objects
+		return base.all()
+
+class AnonymousCircularStreetHandler(AnonymousBaseHandler):
 	allow_methods = ('GET',)
 	model = CircularStreet 
 
@@ -47,7 +81,18 @@ class CircularStreetHandler(BaseHandler):
 		base = CircularStreet.objects
 		return base.all()
 
-class TimeStreetHandler(BaseHandler):
+class CircularStreetHandler(BaseHandler):
+	allow_methods = ('GET',)
+	model = CircularStreet 
+	anonymous = AnonymousCircularStreetHandler
+	fields = ('id', 'year', 'name', 'order', 'width', 'distance_from_center', 'street_line')
+
+	@classmethod
+	def read(self, request):
+		base = CircularStreet.objects
+		return base.all()
+
+class AnonymousTimeStreetHandler(AnonymousBaseHandler):
 	allow_methods = ('GET',)
 	model = TimeStreet
 
@@ -58,10 +103,32 @@ class TimeStreetHandler(BaseHandler):
 		base = TimeStreet.objects
 		return base.all()
 
-class YearHandler(BaseHandler):
+class TimeStreetHandler(BaseHandler):
+	allow_methods = ('GET',)
+	model = TimeStreet
+	anonymous = AnonymousTimeStreetHandler
+	fields = ('id', 'year', 'hour', 'minute', 'name', 'width', 'street_line')
+
+	@classmethod
+	def read(self, request):
+		base = TimeStreet.objects
+		return base.all()
+
+class AnonymousYearHandler(AnonymousBaseHandler):
 	allow_methods = ('GET',)
 	model = Year   
 
+	fields = ('id', 'location', 'location_point', 'participants', 'theme')
+
+	@classmethod
+	def read(self, request):
+		base = Year.objects
+		return base.all()
+
+class YearHandler(BaseHandler):
+	allow_methods = ('GET',)
+	model = Year   
+	anonymous = AnonymousYearHandler
 	fields = ('id', 'location', 'location_point', 'participants', 'theme')
 
 	@classmethod
