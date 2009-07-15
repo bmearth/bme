440dad8e1e42741fb9158695f926e4ad57c6afd4
Craig Steinberger <cjs@readyfireaim.org>
2009-07-15 15:39:53 -0400

    Change form length of title field on Event

--- apps/swingtime/models.py
+++ apps/swingtime/models.py
@@ -54,7 +54,7 @@ class Event(models.Model):
     '''
     Container model for general metadata and associated ``Occurrence`` entries.
     '''
-    title = models.CharField(max_length=32)
+    title = models.CharField(max_length=50)
     description = models.CharField(max_length=2000)
     event_type = models.ForeignKey(EventType)
     notes = generic.GenericRelation(Note)
