50d698ef8bf8bcda67a4234ed26b10e9bfcf168d
bme@earthdev.burningman.com <bme@earthdev.burningman.com>
2009-07-24 21:21:10 -0700

    Added occurrences to event API output

--- apps/api/handlers.py
+++ apps/api/handlers.py
@@ -3,12 +3,13 @@ from piston.handler import BaseHandler, AnonymousBaseHandler
 from piston.emitters import Emitter, JSONEmitter
 from brc.models import *
 from api.emitters import GeoJSONEmitter
+from swingtime.models import Event, Occurrence
 
 JSONEmitter.unregister('json')
 Emitter.register('json', GeoJSONEmitter, content_type='text/javascript; charset=utf-8')
 
 art_fields = ('id', 'name', 'year', 'slug', 'artist', 'description', 'url', 'contact_email', 'location_point', 'location_poly', 'circular_street', 'time_address')
-event_fields = ('id', 'title', 'description', 'print_description', 'year', 'slug', 'event_type', 'hosted_by_camp', 'located_at_art', 'other_location', 'check_location', 'url', 'contact_email', 'location_point', 'location_track', 'all_day')
+event_fields = ('id', 'title','description', 'print_description', 'year', 'slug', 'event_type', 'hosted_by_camp', 'located_at_art', 'other_location', 'check_location', 'url', 'contact_email', 'location_point', 'location_track', 'all_day', ('occurrence_set', ('start_time', 'end_time')))
 camp_fields = ('id', 'year', 'name', 'description', 'type', 'start_date_time', 'end_date_time', 'duration', 'repeats', 'hosted_by_camp', 'located_at_art', 'location_point', 'location_poly', 'url', 'contact_email') 
 cstreet_fields = ('id', 'year', 'name', 'order', 'width', 'distance_from_center', 'street_line')
 tstreet_fields = ('id', 'year', 'hour', 'minute', 'name', 'width', 'street_line')
@@ -29,22 +30,22 @@ class ArtInstallationHandler(BaseHandler):
 class AnonymousPlayaEventHandler(AnonymousBaseHandler):
 	allow_methods = ('GET',)
 	model = PlayaEvent 
-	fields = event_fields
-
-class PlayaEventHandler(BaseHandler):
-	allow_methods = ('GET',)
-	model = PlayaEvent 
-	fields = event_fields
-	anonymous = AnonymousPlayaEventHandler
+	fields = event_fields 
 
 	def read(self, request, year_year=None):
-		base = PlayaEvent.objects
+		base = PlayaEvent.objects.filter(moderation='A', list_online=True)
 		if(year_year):
 		        year = Year.objects.get(year=year_year)
         		events = PlayaEvent.objects.filter(year=year)
 			return events
 		else:
 			return base.all()
+
+class PlayaEventHandler(BaseHandler):
+	allow_methods = ('GET',)
+	model = PlayaEvent 
+	anonymous = AnonymousPlayaEventHandler
+	fields = event_fields
 
 class AnonymousThemeCampHandler(AnonymousBaseHandler):
 	allow_methods = ('GET',)
