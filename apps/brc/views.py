09d2716f9146763c2452c6a772e49a860132482e
Craig Steinberger <cjs@readyfireaim.org>
2009-08-02 20:50:04 -0400

    Add Playa Event CVS export views and urls

--- apps/brc/views.py
+++ apps/brc/views.py
@@ -1,4 +1,5 @@
 import math
+import csv
 
 from datetime import datetime, date, time
 from django.shortcuts import get_object_or_404, render_to_response
@@ -8,6 +9,7 @@ from django.template import RequestContext
 from django.contrib.gis.geos import *
 from django.contrib.auth.decorators import login_required
 from django.views.generic.create_update import delete_object
+from django.db.models import Count
 
 from swingtime.models import Event, Occurrence
 from swingtime import utils, forms
@@ -426,6 +428,198 @@ def delete_occurrence(request,
 		)
 	
 
+def _map_to_ascii(t):
+  punctuation = { 0x2018:0x27, 0x2019:0x27, 0x201C:0x22, 0x201D:0x22 }
+  new_string=t.translate(punctuation).encode('ascii', 'xmlcharrefreplace')
+  return new_string
+    
+            
+@login_required
+def csv_onetime(request, year_year):
+  year= Year.objects.filter(year=year_year)
+  events = PlayaEvent.objects.filter(year=year, moderation='A').order_by('id').annotate(num_occurrences=Count('occurrence'))[:980]
+  timed_events= itertools.ifilter(lambda e: e.all_day==False, events)
+  
+  onetime_events = list(itertools.ifilter(lambda e: e.num_occurrences==1, timed_events))
+  
+  # Create the HttpResponse object with the appropriate CSV header.
+  response = HttpResponse(mimetype='text/csv')
+  response['Content-Disposition'] = 'attachment; filename=onetime_events.csv'
+
+  writer = csv.writer(response)
+  writer.writerow(['Title', 'Description', 'Start Date', 'Start Time', 'End Date', 'End Time', 'Location', 'Placement', 'Event Type'])
+  for e in onetime_events:
+    o = e.next_occurrence()
+    title = _map_to_ascii(e.title)
+    start_date = o.start_time.strftime('%A')
+    if o.start_time.day == 7:
+      start_date = 'Lastday'
+    start_time = o.start_time.strftime('%H:%M')
+    end_date = o.end_time.strftime('%a %b %d')
+    if o.end_time.day == 7:
+      end_date = 'Lastday'
+    end_time = o.end_time.strftime('%H:%M')
+    print_description = _map_to_ascii(e.print_description)
+    placement_location = ''
+    if e.check_location:
+      location = 'Check @ Play Info'
+      placement_location = 'Check @ Playa Info'
+    if e.other_location:
+      location = e.other_location
+    elif e.located_at_art:
+      location = e.located_at_art.name
+      placement_location = e.located_at_art.location_string
+    else:
+      location = e.hosted_by_camp.name
+      placement_location = e.hosted_by_camp.location_string
+    event_type = e.event_type
+    writer.writerow([title, print_description, start_date, start_time, end_date, end_time, location, placement_location, event_type])
+    
+  return response
+  
+@login_required
+def csv_repeating(request, year_year):
+  year= Year.objects.filter(year=year_year)
+  events = PlayaEvent.objects.filter(year=year, moderation='A').order_by('id').annotate(num_occurrences=Count('occurrence'))[:980]
+  timed_events= itertools.ifilter(lambda e: e.all_day==False, events)
+  
+  repeating_events = list(itertools.ifilter(lambda e: e.num_occurrences>1, timed_events))
+
+  # Create the HttpResponse object with the appropriate CSV header.
+  response = HttpResponse(mimetype='text/csv')
+  response['Content-Disposition'] = 'attachment; filename=repeating_events.csv'
+
+  writer = csv.writer(response)
+  writer.writerow(['Title', 'Description', 'Start Date', 'Start Time', 'End Date', 'End Time', 'Location', 'Placement', 'Event Type'])
+  for e in repeating_events:
+    occurrences = e.upcoming_occurrences().order_by('start_time')
+    title = _map_to_ascii(e.title)
+    print_description = _map_to_ascii(e.print_description)
+    placement_location = ''
+    if e.check_location:
+      location = 'Check @ Play Info'
+      placement_location = 'Check @ Playa Info'
+    if e.other_location:
+      location = e.other_location
+    elif e.located_at_art:
+      location = e.located_at_art.name
+      placement_location = e.located_at_art.location_string
+    else:
+      location = e.hosted_by_camp.name
+      placement_location = e.hosted_by_camp.location_string
+    event_type = e.event_type
+    for o in occurrences:
+      start_date = o.start_time.strftime('%A')
+      if o.start_time.day == 7:
+        start_date = 'Lastday'
+      start_time = o.start_time.strftime('%H:%M')
+      end_date = o.end_time.strftime('%a %b %d')
+      if o.end_time.day == 7:
+        end_date = 'Lastday'
+      end_time = o.end_time.strftime('%H:%M')
+
+      writer.writerow([title, print_description, start_date, start_time, end_date, end_time, location, placement_location, event_type])[:980]
+      # Blank out the descriptive items of the event so we only have data data for the repeat occurrences
+      title=''
+      print_description = ''
+      location = ''
+      placement_location=''
+      event_type=''
+
+  return response
+  
+@login_required
+def csv_all_day_onetime(request, year_year):
+  year= Year.objects.filter(year=year_year)
+  events = PlayaEvent.objects.filter(year=year, moderation='A').order_by('id').annotate(num_occurrences=Count('occurrence'))[:980]
+  all_day_events= itertools.ifilter(lambda e: e.all_day==True, events)
+
+  onetime_all_day_events = list(itertools.ifilter(lambda e: e.num_occurrences==1, all_day_events))
+
+  # Create the HttpResponse object with the appropriate CSV header.
+  response = HttpResponse(mimetype='text/csv')
+  response['Content-Disposition'] = 'attachment; filename=all_day_onetime_events.csv'
+
+  writer = csv.writer(response)
+  writer.writerow(['Title', 'Description', 'Start Date', 'Location', 'Placement', 'Event Type'])
+  for e in onetime_all_day_events:
+    occurrences = e.upcoming_occurrences().order_by('start_time')
+    title = _map_to_ascii(e.title)
+    print_description = _map_to_ascii(e.print_description)
+    placement_location = ''
+    if e.check_location:
+      location = 'Check @ Play Info'
+      placement_location = 'Check @ Playa Info'
+    if e.other_location:
+      location = e.other_location
+    elif e.located_at_art:
+      location = e.located_at_art.name
+      placement_location = e.located_at_art.location_string
+    else:
+      location = e.hosted_by_camp.name
+      placement_location = e.hosted_by_camp.location_string
+    event_type = e.event_type
+    for o in occurrences:
+      start_date = o.start_time.strftime('%A')
+      if o.start_time.day == 7:
+        start_date = 'Lastday'
+
+      writer.writerow([title, print_description, start_date, location, placement_location, event_type])
+      # Blank out the descriptive items of the event so we only have data data for the repeat occurrences
+      title=''
+      print_description = ''
+      location = ''
+      placement_location=''
+      event_type=''
+
+  return response
+  
+@login_required
+def csv_all_day_repeating(request, year_year):
+  year= Year.objects.filter(year=year_year)
+  events = PlayaEvent.objects.filter(year=year, moderation='A').order_by('id').annotate(num_occurrences=Count('occurrence'))
+  all_day_events= itertools.ifilter(lambda e: e.all_day==True, events)
+
+  repeating_events = list(itertools.ifilter(lambda e: e.num_occurrences>1, all_day_events))
+
+  # Create the HttpResponse object with the appropriate CSV header.
+  response = HttpResponse(mimetype='text/csv')
+  response['Content-Disposition'] = 'attachment; filename=all_day_repeating_events.csv'
+
+  writer = csv.writer(response)
+  writer.writerow(['Title', 'Description', 'Start Date',  'Location', 'Placement', 'Event Type'])
+  for e in repeating_events:
+    occurrences = e.upcoming_occurrences().order_by('start_time')
+    title = _map_to_ascii(e.title)
+    print_description = _map_to_ascii(e.print_description)
+    placement_location = ''
+    if e.check_location:
+      location = 'Check @ Play Info'
+      placement_location = 'Check @ Playa Info'
+    if e.other_location:
+      location = e.other_location
+    elif e.located_at_art:
+      location = e.located_at_art.name
+      placement_location = e.located_at_art.location_string
+    else:
+      location = e.hosted_by_camp.name
+      placement_location = e.hosted_by_camp.location_string
+    event_type = e.event_type
+    for o in occurrences:
+      start_date = o.start_time.strftime('%A')
+      if o.start_time.day == 7:
+        start_date = 'Lastday'
+
+
+      writer.writerow([title, print_description, start_date, location, placement_location, event_type])
+      # Blank out the descriptive items of the event so we only have data data for the repeat occurrences
+      title=''
+      print_description = ''
+      location = ''
+      placement_location=''
+      event_type=''
+
+  return response
 #-------------------------------------------------------------------------------
 #---------- Misc ----------
 #-------------------------------------------------------------------------------
