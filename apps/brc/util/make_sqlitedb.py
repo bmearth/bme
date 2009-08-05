4df242e3d4d88b6a64074653d0836bdfc009d30d
bme@earthdev.burningman.com <bme@earthdev.burningman.com>
2009-08-04 22:42:26 -0700

    Minor changes to make_sqlitedb.py and latest sqlite DB Export

--- apps/brc/util/make_sqlitedb.py
+++ apps/brc/util/make_sqlitedb.py
@@ -2,10 +2,10 @@ import os, sys
 import psycopg2
 from pysqlite2 import dbapi2 as sqlite
 
-sys.path.append('/home/ortelius/projects/mobitag/src/pinax/apps')
-sys.path.append('/home/ortelius/projects/mobitag/src')
-sys.path.append('/home/ortelius/projects/bme')
-sys.path.append('/home/ortelius/projects/bme/bme/apps')
+sys.path.append('/home/bme/src/pinax/apps')
+sys.path.append('/home/bme/src')
+sys.path.append('/home/bme/src/bme')
+sys.path.append('/home/bme/src/bme/apps')
 os.environ['DJANGO_SETTINGS_MODULE'] ='bme.settings'
 
 from brc.models import *
@@ -18,16 +18,20 @@ cursor = connection.cursor()
 try:
 	cursor.execute('delete from theme_camp')
 except:
-	cursor.execute("CREATE TABLE IF NOT EXISTS 'theme_camp' ('pk' INTEGER NOT NULL, 'name' TEXT, 'year' INTEGER, 'description' TEXT, 'url' TEXT, 'contact_email' TEXT, 'hometown' TEXT, 'circular_street' INTEGER, 'time_address' TEXT, 'latitude' FLOAT, 'longitude' FLOAT, 'image_url' TEXT)")
+	cursor.execute("CREATE TABLE IF NOT EXISTS 'theme_camp' ('pk' INTEGER NOT NULL, 'name' TEXT, 'year' INTEGER, 'description' TEXT, 'url' TEXT, 'contact_email' TEXT, 'hometown' TEXT, 'location' TEXT, 'circular_street' INTEGER, 'time_address' TEXT, 'latitude' FLOAT, 'longitude' FLOAT, 'image_url' TEXT)")
 
 
 camps = ThemeCamp.objects.filter(year=4).extra(select={'lower_name': 'lower(name)'}).order_by('lower_name')
 
+count = 0
 for camp in camps:
 	try:
-		sql = 'INSERT INTO theme_camp (pk, name, year,  description, url, contact_email, hometown, circular_street, time_address, latitude, longitude, image_url) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)' 
-		cursor.execute(sql,(camp.id, camp.name, int(camp.year.year), camp.description, camp.url, camp.contact_email, camp.hometown, camp.circular_street, camp.time_address, 1.0, 1.0, str(camp.image)))
+		sql = 'INSERT INTO theme_camp (pk, name, year,  description, url, contact_email, hometown, location, circular_street, time_address, latitude, longitude, image_url) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)' 
+		cursor.execute(sql,(count, camp.name, int(camp.year.year), camp.description, camp.url, camp.contact_email, camp.hometown, camp.location_string, camp.circular_street, camp.time_address, 1.0, 1.0, str(camp.image)))
 		connection.commit()
+		count += 1
+	except sqlite.OperationalError, msg:
+		print msg
 	except:
 		print sys.exc_info()[0]
 
@@ -38,11 +42,13 @@ except:
 
 art_installs = ArtInstallation.objects.filter(year=4).extra(select={'lower_name': 'lower(name)'}).order_by('lower_name')
 
+count = 0
 for art in art_installs:
 	try:
 		sql = 'INSERT INTO art_install (pk, year, name, slug, artist, description, url, contact_email, image_url) VALUES (?,?, ?, ?, ?, ?,?, ?, ?)' 
-		cursor.execute(sql,(art.id, int(art.year.year), art.name, art.slug, art.artist, art.description, art.url, art.contact_email, str(art.image)))
+		cursor.execute(sql,(count, int(art.year.year), art.name, art.slug, art.artist, art.description, art.url, art.contact_email, str(art.image)))
 		connection.commit()
+		count += 1
 	except:
 		print sys.exc_info()[0]
 
